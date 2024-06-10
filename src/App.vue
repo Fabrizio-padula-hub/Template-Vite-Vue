73a79830e2f628fc4e6679a6fa3f25d6fd1ba3e1
Fabrizio-padula <fabriziopadula80@gmail.com>
2024-06-10 18:56:18 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<!-- Collegato già lo style generic con le variables e collegato tramite CDN Bootstrap -->
+<!-- Cancellato già tutto quello che non serve -->
+<!-- Pronto per partire -->
+<script>
+export default{
+
+}
+</script>
+
+
+<template>
+    <h1>prova</h1>
+</template>
+
+
+<style lang="scss">
+// importare lo style generico
+@use './style/generic';
+// importare le variabili
+@use './style/partials/variables' as *;
+</style>
